82f1e029fb599539dacab5760ff8c5fe45292f59
Анатолий <69402978+talynik@users.noreply.github.com>
2021-06-18 21:53:03 +0300

    aboutProject

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -4,12 +4,23 @@ import {withRouter} from "react-router-dom";
 function AboutProject() {
 
   return (
-    <section className="main__promo">
-      <div className="main__promo-block">
-        <div className="main__promo_block-text">
-          <h1 className="main__promo_block-title">Учебный проект студента факультета Веб-разработки.</h1>
-          <p className="main__promo_block-subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+    <section className="main__aboutProject">
+      <h2 className="main__section_header">О проекте</h2>
+      <div className="main__aboutProject_block">
+        <div className="main__aboutProject_stage">
+          <h3 className="main__aboutProjec_title">Дипломный проект включал 5 этапов</h3>
+          <p className="main__aboutProject_subtitle">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </div>
+        <div className="main__aboutProject_time">
+          <h3 className="main__aboutProjec_title">На выполнение диплома ушло 5 недель</h3>
+          <p className="main__aboutProject_subtitle">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
+      <div className="main__aboutProject_shema">
+        <p className="main__aboutProject_shema-type1">1 неделя</p>
+        <p className="main__aboutProject_shema-type2">4 неделя</p>
+        <p className="main__aboutProject_shema-type3">Back-end</p>
+        <p className="main__aboutProject_shema-type3">Front-end</p>
       </div>
     </section>
   );
