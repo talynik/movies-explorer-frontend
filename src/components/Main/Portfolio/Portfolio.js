dec27645ef8f8968a8f9a851a5053bc012d5f2e8
Анатолий <69402978+talynik@users.noreply.github.com>
2021-06-20 19:54:19 +0300

    Portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -7,14 +7,20 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h3 className="portfolio__header">Портфолио</h3>
-      <div className="portfolio__links">
-        <a className="portfolio__link" href="https://www.facebook.com/talynik">Статичный сайт<img className="portfolio_picture" src={link} alt="Ссылка"></img></a>
-        <img className="portfolio__picture" src={link} alt="Ссылка"></img>
-        <a className="portfolio__link_line" href="https://www.facebook.com/talynik">Адаптивный сайт</a>
-        <img className="portfolio__picture_line" src={link} alt="Ссылка"></img>
-        <a className="portfolio__link_line" href="https://www.facebook.com/talynik">Одностраничное приложение</a>
-        <img className="portfolio__picture_line" src={link} alt="Ссылка"></img>
-      </div>
+      <ul className="portfolio__links">
+        <li className="portfolio__block">
+          <a className="portfolio__link" href="https://talynik.github.io/how-to-learn/index.html">Статичный сайт</a>
+          <img className="portfolio__picture" src={link} alt="Статичный сайт"></img>
+        </li>
+        <li className="portfolio__block portfolio__block-line">
+          <a className="portfolio__link" href="https://talynik.github.io/russian-travel/index.html">Адаптивный сайт</a>
+          <img className="portfolio__picture" src={link} alt="Адаптивный сайт"></img>
+        </li>
+        <li className="portfolio__block portfolio__block-line">
+          <a className="portfolio__link" href="https://talynik.student.nomoredomains.club">Одностраничное приложение</a>
+          <img className="portfolio__picture" src={link} alt="Одностраничное приложение"></img>
+        </li>
+      </ul>
     </section>
   );
 }
